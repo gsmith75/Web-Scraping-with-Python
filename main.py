499000801136194db0240c8a9e888eb0e166e855
Gustavo Marques <gustavom.gmarues@gmail.com>
2023-11-14 22:12:29 -0300

    Alteraçao em item_informations (Coloquei todos utilizando try-except) e na última funçao (em caso de results ser uma lista vazia

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ def search_term(term):
 # This function is the responsible for analyzing the HTML of the sit.
 def item_informations(item): 
     # Find the item's title.
-    exist_title  = item.find('h2', class_ = "ui-search-item__title")
-    if exist_title:
-        title = exist_title.text
-    else:
-        title = "None"
-
+    
+    try:
+        title = item.find('h2', class_ = "ui-search-item__title").text
+    except AttributeError:
+        title = 'None'
+    
     # Finde the item's price.
+
     price_integer = item.find('span', class_ = "andes-money-amount__fraction").text
-    find_cents = item.find('span', class_ = "andes-money-amount__cents andes-money-amount__cents--superscript-24")
-    if find_cents:
-        price = price_integer + ',' + find_cents.text
-    else:
+    try:
+        find_cents = item.find('span', class_ = "andes-money-amount__cents andes-money-amount__cents--superscript-24").text
+        price = price_integer + ',' + find_cents
+    except AttributeError:
         price = price_integer + ',' + '00'
 
     # Find the item's rating value.
@@ -43,6 +44,7 @@ def item_informations(item):
 
 # This is the function responsible for making the request.
 # Also, this function turn the result into a panda table.
+
 def get_doc(url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"}
     page = requests.get(url, headers=headers)
@@ -52,16 +54,28 @@ def get_doc(url):
 
     # Find the HTML element that contains all the products.
     results = soup.find_all("ol", attrs={'class': 'ui-search-layout ui-search-layout--grid'})
-    for item in results:
-        record = item_informations(item)
-        if record:
-            records.append(record)
 
+    # A try-except block to deal with differents type of results in the HTML.
+    try:
+        if not results:
+            raise ValueError
+        for item in results:
+            record = item_informations(item)
+            if record:
+                records.append(record)
+    except ValueError:
+        results = soup.find_all("li", attrs={'class': 'ui-search-layout__item'})
+        for item in results:
+            record = item_informations(item)
+            if record:
+                records.append(record)
+
+    # Create a Pandas table.
     df = pd.DataFrame(records, columns=['Title', 'Price', 'Rating', 'Reviews'])
     return df
 
 
-site = search_term('mala')
+site = search_term('smartphone')
 print(get_doc(site))
 
     
